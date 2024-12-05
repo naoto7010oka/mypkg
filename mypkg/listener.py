c20fbe6a0cc4cbb30536e3105c076f21116f7756
naoto <naoto1221g@gmail.com>
2024-12-05 12:31:28 +0900

    Add a command

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -3,15 +3,31 @@ from rclpy.node import Node
 from std_msgs.msg import Int16
 
 
-rclpy.init()
-node = Node("listener")
+class ListenerNode(Node):
+    def __init__(self):
+        super().__init__('listener')  
+        
+        self.subscription = self.create_subscription(Int16,'countup',self.callback, 10)
+
+    def callback(self, msg):
+        
+        self.get_logger().info(f'Listen: {msg.data}')
 
 
-def cb(msg):
-    global node
-    node.get_logger().info("Listen: %d" % msg.data)
+def main(args=None):
+    
+    rclpy.init(args=args)
+    
+    node = ListenerNode()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        
+        node.get_logger().info('Shutting down node.')
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
 
 
-def main():
-    pub = node.create_subscription(Int16, "countup", cb, 10)
-    rclpy.spin(node)
+if __name__ == '__main__':
+    main()
